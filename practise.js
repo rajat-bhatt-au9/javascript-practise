c71049b4235508fc94b5ecdbfea55484a686cbb3
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2021-01-29 19:37:26 +0530

    practise objects

--- practise.js
+++ practise.js
@@ -1,23 +1,9 @@
-/* let const and var */
-
-const name = "Rajat";
-let age = 24;
-const hasHobbie = true;
-
-age = 32;
-
-const summarizeUser = (userName, userAge, userHasHobby) => {
-  return (
-    "Name is " +
-    userName +
-    ", age is " +
-    userAge +
-    " and the user has hobbies: " +
-    userHasHobby
-  );
+const person = {
+  name: "Rajat Bhatt",
+  age: 34,
+  greet() {
+    console.log("Hi my name is " + this.name);
+  },
 };
 
-const add = (n) => n + 1;
-
-/* console.log(summarizeUser(name, age, hasHobbie)); */
-console.log(add(5));
+person.greet();
